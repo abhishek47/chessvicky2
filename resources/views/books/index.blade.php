a280dd82f06ce635358d1bccc63de6db347d3dfa
Abhishek Wani <Trumpets@Abhisheks-iMac.local>
2017-04-14 23:50:00 +0530

    Admin Panel Begining

---
+++ resources/views/books/index.blade.php
@@ -0,0 +1,59 @@
+@extends('layouts.app')
+
+
+@section('content')
+
+
+ <!-- start page-title -->
+        <section class="page-title">
+            <div class="container">
+                <div class="title-box">
+                    <h1>Checkmate Puzzles</h1>
+                    <ol class="breadcrumb">
+                        <li><a href="#">Home</a></li>
+                        <li>Puzzles</li>
+                    </ol>
+                </div>
+            </div> <!-- end container -->
+        </section>
+        <!-- end page-title -->
+
+
+        <!-- start case-studies -->
+        <section class="case-studies section-padding">
+            <div class="container">        
+
+               <div class="row">
+                    <div class="col col-xs-12">
+                        
+                            <a class="btn btn-primary" style="margin-bottom: 19px;" href="/puzzles/random"><b>Solve Random Puzzles</b></a>
+                      
+                    </div>
+
+                 
+                </div>
+                <div class="row case-studies-grids">
+
+                  @foreach($puzzles as $puzzle)
+                    
+                     @include('puzzles.puzzle')
+
+
+                   @endforeach 
+                    
+                </div> <!-- end row -->
+
+                <div class="row">
+                    <div class="col col-xs-12">
+                        
+                            {{ $puzzles->links() }}
+                      
+                    </div>
+                </div>
+            </div> <!-- end container -->
+        </section>
+        <!-- end case-studies -->
+
+
+
+@endsection
