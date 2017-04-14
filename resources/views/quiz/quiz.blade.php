f4232c00491b6efa4e724eaf192959c521a9c2e9
Abhishek Wani <Trumpets@Abhisheks-iMac.local>
2017-04-14 21:35:11 +0530

    QUiz

--- resources/views/quiz/quiz.blade.php
+++ resources/views/quiz/quiz.blade.php
@@ -3,8 +3,8 @@
     <div class="grid">
         <div class="details">
             <h3><a href="/quiz/{{ $quiz->id }}">{{ $quiz->title }}</a></h3>
-            <span>Level : {{$quiz->level}} | Points : {{$quiz->getPoints()}} | Coins : {{$quiz->getCoins()}}</span> <br><br>
-            <b><a href="/quiz/{{ $quiz->id }}" class="view-case-studes" style="font-size: 22px;">Solve Quiz --></a></b>
+            <span>Level : {{$quiz->level}} | Points : {{$quiz->getPoints()}} | Coins : {{$quiz->getCoins()}} | Questions : {{ count($quiz->questions) }}</span> <br><br>
+            <b><a href="/quiz/{{ $quiz->id }}" class="view-case-studes" style="font-size: 21px;color: #f39c12;">Solve Quiz --></a></b>
         </div>
     </div>
 </div>
