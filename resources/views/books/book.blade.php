8215988c50055758c52f8d6899f944381462ea3b
Abhishek Wani <Trumpets@Abhisheks-iMac.local>
2017-04-15 00:41:12 +0530

    Admin Panel Begining

--- resources/views/books/book.blade.php
+++ resources/views/books/book.blade.php
@@ -1,6 +1,6 @@
 <div class="col col-md-4 col-xs-6">
-    <div class="grid panel panel panel-default" style="padding: 10px 10px 10px 10px;box-shadow: 3px 3px 3px 3px rgba(0,0,0,.05);background: #f7f7f7;">  
-     <div class="panel-heading"></div>
+    <div class="grid panel panel panel-default" style="padding: 10px 10px 10px 10px;box-shadow: 3px 3px 3px 3px rgba(0,0,0,.05);background: #dbffbc;">  
+     <div class="panel-heading" style="background: #2e6300"></div>
         <div class="img-holder">
             <a href="/books/{{ $book->id }}">
                 <img src="{{ $book->photo }}" style="height: 400px;width: 100%;" class="img img-responsive">
