f6b60f2b294290cd18028f5cddd4d2f754f6a4ea
Abhishek Wani <Trumpets@Abhisheks-iMac.local>
2017-04-16 00:34:54 +0530

    Article

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -33,7 +33,7 @@
     							<div class="comments">
     							 <ol>
 		                            @include('posts.article')
-		                            <hr>
+		                            
 		                          </ol>  
                                 </div>
 	                           @endforeach 
