2d3e4aa020f4c2c107b0a2f95c6a5fe58c7e9bce
Abhishek Wani <Trumpets@Abhisheks-iMac.local>
2017-04-16 07:32:34 +0530

    articles

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -10,6 +10,17 @@ use App\Post;
 class PagesController extends Controller
 {
     
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('guest');
+    }
+    
+
     public function index()
     {
     	$puzzles = Puzzle::latest()->limit(3)->get();
