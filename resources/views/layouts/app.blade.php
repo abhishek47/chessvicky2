c4669a1941c3bf9724650cc1b2d4205b120a862d
Abhishek Wani <Trumpets@Abhisheks-iMac.local>
2017-05-09 11:55:34 +0530

    Register

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -160,8 +160,14 @@
                             @else
                                 <li class="sub-menu">
                                     <a href="/home" style="padding-top: 25px;">
-                                     
-                                        <img style="display: inline;width: 30px;height: 30px;margin-right: 5px;margin-bottom: 2px;" src="/images/testimonials/img-1.jpg" alt class="img img-responsive img-circle">
+
+                                     @if( Auth::user()->profile->photo)
+                                      <img class="img img-responsive" style="display: inline;width: 30px;height: 30px;margin-right: 5px;margin-bottom: 2px;" src="/{{  Auth::user()->profile->photo }}" alt="" class="img img-responsive img-circle">
+                                    @else
+                                     <span class="chatter_avatar_circle small" style="background-color:#<?= \DevDojo\Chatter\Helpers\ChatterHelper::stringToColorCode( Auth::user()->name) ?>">
+                                          {{ ucfirst(substr( Auth::user()->name, 0, 1)) }}
+                                      </span>
+                                     @endif 
                                          {{ Auth::user()->name }} <span class="caret"></span>
                                     </a>
 
