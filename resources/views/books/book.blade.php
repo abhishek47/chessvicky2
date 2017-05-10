40b11a6b3da27e16652462f3f2c28c3e24c9cc1a
Abhishek Wani <Trumpets@Abhisheks-iMac.local>
2017-05-10 14:08:39 +0530

    Game

--- resources/views/books/book.blade.php
+++ resources/views/books/book.blade.php
@@ -1,8 +1,8 @@
 <div class="col col-md-4 col-xs-6">
-    <div class="grid panel panel panel-default" style="padding: 10px 10px 10px 10px;box-shadow: 3px 3px 3px 3px rgba(0,0,0,.05);background: #f7f7f7;height: 610px;">  
+    <div class="grid panel panel panel-default" style="padding: 10px 10px 10px 10px;box-shadow: 3px 3px 3px 3px rgba(0,0,0,.05);background: #f7f7f7;height: 620px;">  
      <div class="panel-heading" style="background: #2e6300"></div>
         <div class="img-holder">
-            <a href="/books/{{ $book->id }}">
+            <a href="{{ $book->link }}" target="_blank">
                 <img src="{{ $book->photo }}" style="height: 400px;width: 100%;" class="img img-responsive">
             </a>
         </div>
@@ -23,7 +23,7 @@
 
         ?>
         <div class="details">
-            <h3 style="font-size: 18px;"><a href="{{ $book->link }}">{{ str_limit($value, $limit = 100, $end = '...') }}</a></h3>
+            <h3 style="font-size: 18px;"><a href="{{ $book->link }}" target="_blank">{{ str_limit($value, $limit = 100, $end = '...') }}</a></h3>
             <span>Price : {{ $book->price }}</span>
             <span>Author : @if(isset($q)){!! highlight_words($book->author, explode(' ', $q)) !!}@else {{ $book->author  }}@endif</span>
             <a href="{{ $book->link }}" class="view-case-studes">Buy Now</a>
