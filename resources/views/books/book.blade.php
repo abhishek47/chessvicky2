6142bc2872ee2cd84c23b838e6125da78f127f33
Abhishek Wani <Trumpets@Abhisheks-iMac.local>
2017-05-10 14:06:50 +0530

    Game

--- resources/views/books/book.blade.php
+++ resources/views/books/book.blade.php
@@ -1,5 +1,5 @@
 <div class="col col-md-4 col-xs-6">
-    <div class="grid panel panel panel-default" style="padding: 10px 10px 10px 10px;box-shadow: 3px 3px 3px 3px rgba(0,0,0,.05);background: #f7f7f7;height: 600px;">  
+    <div class="grid panel panel panel-default" style="padding: 10px 10px 10px 10px;box-shadow: 3px 3px 3px 3px rgba(0,0,0,.05);background: #f7f7f7;height: 610px;">  
      <div class="panel-heading" style="background: #2e6300"></div>
         <div class="img-holder">
             <a href="/books/{{ $book->id }}">
